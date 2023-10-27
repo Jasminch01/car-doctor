ebc64015306b132bc132e185e610491f592ca90f
jasmin <jasminchakma895@gmail.com>
2023-10-27 10:54:13 +0600

    displayed services services-card components

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import ServiceCard from "../ServiceCard/ServiceCard";
 
 const Services = () => {
     const [services, setServices] = useState([]);
@@ -11,9 +12,21 @@ const Services = () => {
     },[])
     console.log(services)
     return (
-        <div>
-            
+        <div className="mt-4">
+        <div className="text-center">
+            <h3 className="text-2xl font-bold text-orange-600">Our Services</h3>
+            <h2 className="text-5xl">Our Service Area</h2>
+            <p>the majority have suffered alteration in some form, by injected humour, or randomised <br /> words which do not look even slightly believable. </p>
         </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {
+                services.map(service => <ServiceCard
+                    key={service._id}
+                    service={service}
+                ></ServiceCard>)
+            }
+        </div>
+    </div>
     );
 };
 
