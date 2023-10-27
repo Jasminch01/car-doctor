8dee65ccbf47f1135b79c6f367fc701af3bdf044
jasmin <jasminchakma895@gmail.com>
2023-10-27 10:50:54 +0600

    loaded services data from server

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,5 +1,15 @@
+import { useEffect, useState } from "react";
 
 const Services = () => {
+    const [services, setServices] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/services')
+        .then(res => res.json())
+        .then(data => {
+            setServices(data)
+        })
+    },[])
+    console.log(services)
     return (
         <div>
             
