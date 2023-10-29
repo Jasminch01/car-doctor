4529039cfd20928b3833a6b5449591b0383c51d9
jasmin <jasminchakma895@gmail.com>
2023-10-29 12:30:56 +0600

    service,service details added

---
+++ src/Components/ServiceDetails/ServiceDetails.jsx
@@ -0,0 +1,66 @@
+import { useLoaderData } from "react-router-dom";
+
+const ServiceDetails = () => {
+  const service = useLoaderData();
+  const { title, _id, price, img } = service;
+  return (
+    <div>
+      <div>
+        <h2 className="text-center text-3xl">Book Service: {title} </h2>
+        <form >
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                // defaultValue={user?.displayName}
+                name="name"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Date</span>
+              </label>
+              <input type="date" name="date" className="input input-bordered" />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="text"
+                name="email"
+                // defaultValue={user?.email}
+                placeholder="email"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Due amount</span>
+              </label>
+              <input
+                type="text"
+                defaultValue={"$" + price}
+                className="input input-bordered"
+              />
+            </div>
+          </div>
+          <div className="form-control mt-6">
+            <input
+              className="btn btn-primary btn-block"
+              type="submit"
+              value="Order Confirm"
+            />
+          </div>
+        </form>
+        <div className="card-body"></div>
+      </div>
+    </div>
+  );
+};
+
+export default ServiceDetails;
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../Layout/Layout";
 import Home from "../Pages/Home/Home";
+import ServiceDetails from "../Components/ServiceDetails/ServiceDetails";
 
 
 const Router = createBrowserRouter([
@@ -11,6 +12,14 @@ const Router = createBrowserRouter([
             {
                 path : '/',
                 element : <Home></Home>
+            },
+            {
+                path : '/services/:id',
+                element : <ServiceDetails></ServiceDetails>,
+                loader : ({params}) => fetch(`http://localhost:5000/services/${params.id}`)
+            },
+            {
+                path : '/mybookings'
             }
         ]
     }
